f8ff4a7ff1b79431a54aed7f5c95d228b3548b46
theJOHNwatt <j.watt10@gmail.com>
2019-09-18 00:51:07 -0700

    routes and links added

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
-
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Movie from './Movies/Movie';
+import MovieList from './Movies/MovieList';
 import SavedList from './Movies/SavedList';
 
 const App = () => {
@@ -12,7 +14,15 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <Route 
+        exact
+        path='/'
+        component={MovieList}
+      />
+      <Route 
+        path="/movies/:id"
+        render={props => <Movie {...props} movie={savedList} />}
+      />
     </div>
   );
 };
